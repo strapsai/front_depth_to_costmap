3faa090045a616f4a1d8dcf11a9b853b9e9f9836
minho0 <alsgh000118@naver.com>
2025-07-22 22:36:14 +0900

    modified

--- docker/autostart_in_container.sh
+++ docker/autostart_in_container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/ros/workspace
 source /opt/ros/humble/setup.bash
 source /home/ros/workspace/install/setup.bash
 ros2 launch depth_to_pointcloud_pub traversability_to_occupancygrid.launch.py
